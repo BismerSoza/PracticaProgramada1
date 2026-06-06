b7fc537d4a9a7ce88f1b87d81cb4e8c4523d801f
Ana Hernández <analidia1025@gmail.com>
2026-06-05 18:12:45 -0600

    Cambios pequeños en traducción de Login

--- PracticaProgramada1/obj/Debug/net10.0/PracticaProgramada1.AssemblyInfo.cs
+++ PracticaProgramada1/obj/Debug/net10.0/PracticaProgramada1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PracticaProgramada1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7ab13c6a7a62f2fdeb7bdd9e6abe1f69632d673")]
 [assembly: System.Reflection.AssemblyProductAttribute("PracticaProgramada1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PracticaProgramada1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
