9bbe84b5940a56c5c354de0a9a6d9cd8b4972583
Jeffry Canales Herrera <canalesjeffry23@gmail.com>
2026-06-05 22:42:19 -0600

    Vista de Dasboard traducido

--- PracticaProgramada1/Controllers/HomeController.cs
+++ PracticaProgramada1/Controllers/HomeController.cs
@@ -11,6 +11,21 @@ namespace PracticaProgramada1.Controllers
             return View();
         }
 
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult GeneralDashboard()
+        {
+            return View();
+        }
+
+        public IActionResult LayoutDefault()
+        {
+            return View();
+        }
+
         [HttpGet]
         public IActionResult Registro()
         {
